e6dc5755e5bb12b32018f93de9ced81ced34f97d
pdpm19 <pdp.moreira@gmail.com>
2022-06-19 16:39:39 +0100

    Finished the i3conf file  and  added some content to README file

--- README.md
+++ README.md
@@ -12,6 +12,19 @@ sudo apt update
 sh zsh.sh
 ```
 
+## I3 keybinding
+Run after setting up i3 for the first time
+```bash
+sh i3conf.sh
+```
+
 ## JupyterLab
 Open `Jupyter.md` file for basic configuration
 
+## DIsclaimer
+- **Use this scripts at your own risk**;
+- Tested with **Pop! OS 22.04 LTS** and **Zorin OS 16 Pro**
+
+## Links
+- [Pop! OS homepage](https://pop.system76.com/)
+- [Zorin homepage](https://zorin.com/os/)
--- i3conf.sh
+++ i3conf.sh
@@ -1,4 +1,13 @@
-# 
+#!/bin/sh
 
+# Use this after running i3 for the first time
 # https://thevaluable.dev/i3-config-mouseless/
-echo 'bindsym $mod+b exec chromium-browser' >>  ~/.config/i3/config
+
+echo 'new_window 1pixel'  >>  ~/.config/i3/config
+echo 'bindsym $mod+b exec google-chrome-stable' >>  ~/.config/i3/config
+echo 'bindsym $mod+m exec arandr' >>  ~/.config/i3/config
+echo 'bindsym $mod+c exec gnome-control-center' >>  ~/.config/i3/config
+echo 'bindsym $mod+n exec nautilus' >>  ~/.config/i3/config
+
+# Reboot to activate these keybinds
+sudo reboot
