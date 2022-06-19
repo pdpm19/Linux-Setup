c19e74daccc45660539188cc0a61d9db7e142d2d
pdpm19 <pdp.moreira@gmail.com>
2022-06-19 16:20:09 +0100

    Added i3 config commands

---
+++ i3conf.sh
@@ -0,0 +1,4 @@
+# 
+
+# https://thevaluable.dev/i3-config-mouseless/
+echo 'bindsym $mod+b exec chromium-browser' >>  ~/.config/i3/config
--- zsh.sh
+++ zsh.sh
@@ -5,7 +5,7 @@ sudo apt install curl zsh mlocate
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # .zshrc aliases
-echo 'alias ..=cd ..' >> ~/.zshrc
+echo 'alias ..=""cd ..""' >> ~/.zshrc
 echo 'alias lt="ls -alt"' >> ~/.zshrc
 echo 'alias nvidia="watch -n 1 nvidia-smi"' >> ~/.zshrc
 echo 'alias sensors="watch -n 1 sensors"' >> ~/.zshrc
